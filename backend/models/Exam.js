fe5a6626a8f8818d49cf1f5c805a263f58b77387
Nowshen Anjuman Khan Prapte <nowshen.anjum@gmail.com>
2025-01-19 00:02:52 +0600

    Update .gitignore files, remove unused components, and enhance Exam and Source models

--- backend/models/Exam.js
+++ backend/models/Exam.js
@@ -6,13 +6,28 @@ const examSchema = new mongoose.Schema({
 		ref: "Source",
 		required: true,
 	},
-	exam_year: { type: Number, required: true },
+	exam_year: {
+		type: Number,
+		required: true,
+		min: 2000,
+		max: new Date().getFullYear,
+	},
 	exam_type: {
 		type: String,
-		enum: ["school_exam", "college_exam", "board_exam", "admission_exam"],
+		enum: [
+			"school_exam",
+			"college_exam",
+			"board_exam",
+			"admission_exam",
+			"government_exam", // সঠিক স্পেলিং
+			"bcs_exam",
+		],
 		required: true,
 	},
 });
+
+// ইনডেক্স যুক্ত করা
+examSchema.index({ source: 1, exam_year: 1, exam_type: 1 });
 
 const Exam = mongoose.model("Exam", examSchema);
 export default Exam;
--- backend/models/Source.js
+++ backend/models/Source.js
@@ -1,13 +1,23 @@
 import mongoose from "mongoose";
 
 const sourceSchema = new mongoose.Schema({
-	name: { type: String, required: true }, // Institution/Board/University name
+	bnName: { type: String, required: true, unique: true }, // Institution/Board/University name
+	enName: { type: String, required: true, unique: true }, // English name of the institution
+	bnAbbreviation: { type: String }, // Institution/Board/University abbreviation
+	enAbbreviation: { type: String, required: true }, // Abbreviation of the institution
 	type: {
 		type: String,
 		enum: ["school", "college", "board", "university"],
 		required: true,
 	}, // বোর্ড, স্কুল, কলেজ নির্ধারণ
-	board: { type: String }, // Board name (if applicable)
+	board: { type: String }, //school or college Board name
+});
+
+sourceSchema.index({
+	bnName: 1,
+	enName: 1,
+	bnAbbreviation: 1,
+	enAbbreviation: 1,
 });
 
 const Source = mongoose.model("Source", sourceSchema);
